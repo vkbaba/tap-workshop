a4c54611f0a39eff7e696d113e91dff775e8243c
vkbaba <26544377+vkbaba@users.noreply.github.com>
2022-09-14 22:26:24 +0900

    Update registry url

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -3,4 +3,4 @@ set -x
 set +e
 
 REGISTRY_USER=${HARBOR_USER:-admin}
-REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry harbor.${INGRESS_DOMAIN} --registry-user $REGISTRY_USER
+REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry harbor.vpantry.net --registry-user $REGISTRY_USER
