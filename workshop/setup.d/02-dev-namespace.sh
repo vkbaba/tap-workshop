211eb60fb55e11f1ff474cac521cdf4414835d6f
vkbaba <26544377+vkbaba@users.noreply.github.com>
2022-09-21 03:30:49 +0900

    Update 02-dev-namespace.sh
    test

--- workshop/setup.d/02-dev-namespace.sh
+++ workshop/setup.d/02-dev-namespace.sh
@@ -4,3 +4,32 @@ set +e
 
 REGISTRY_USER=${HARBOR_USER:-admin}
 REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create registry-credentials --registry harbor.vpantry.net --registry-user $REGISTRY_USER
+
+cat <<'EOF' | kubectl -n $SESSION_NAMESPACE apply -f -
+apiVersion: tekton.dev/v1beta1
+kind: Pipeline
+metadata:
+  name: developer-defined-tekton-pipeline
+  labels:
+    apps.tanzu.vmware.com/pipeline: test
+spec:
+  params:
+  - name: source-url
+  - name: source-revision
+  tasks:
+    - name: test
+      params:
+      - name: source-url
+        value: $(params.source-url)
+      - name: source-revision
+        value: $(params.source-revision)
+      taskSpec:
+        params:
+        - name: source-url
+        - name: source-revision
+        steps:
+        - name: test
+          image: alpine
+          script: |-
+            echo "Skip Test :)"
+EOF
